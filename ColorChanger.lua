4d81a20d77f1e636202e442dc2b4b2ccc4e0170c
alatsombath <alatsombath@users.noreply.github.com>
2015-03-31 22:42:11 -0700

    1.3.8 (22 February 2015)
    Index colors

--- ColorChanger.lua
+++ ColorChanger.lua
@@ -1,118 +1,49 @@
--- ColorChanger v1.3 by Smurfier (smurfier20@gmail.com); Modified
+-- ColorChanger v2.0, A modification of ColorChanger v1.3 by Smurfier (smurfier20@gmail.com)
 -- This work is licensed under a Creative Commons Attribution-Noncommercial-Share Alike 3.0 License.
 
-local ColorsTable,ColorsOptTable,ColorsCheckTable,ColorsVarTable,VarTable,VarNameTable,VarSubTable={},{},{},{},{},{},{}
-local MeasureTable,MeasureSubTable,MeterTable,MinValue,MaxValue,MaxMin,Amp,Option={},{},{},{},{},{},{},{}
-local gmatch,gsub,concat,random=string.gmatch,string.gsub,table.concat,math.random
-
-local function Go(ColorsStr,Init,i,j)
-	local a,b,c={},Init,1
-	
-	local function Replace(d)
-		local a,b={},0 for c in gmatch(d,"[^,]+") do b=b+1
-			if c=="Random" then a[b]=random(0,255) else a[b]=c end end
-		return concat(a,",")
-	end
-	
-	if b==-1 then local e=0 for d in gmatch(ColorsOptTable[j],"[^%|]+") do if e==0 then e=e+1 else a[2]=Replace(d) end end end
-	for d in gmatch(ColorsStr,"[^%|]+") do if b==1 then a[c]=Replace(d) c=c+1 elseif b==-1 then b=b+1 else a[1]=d end end
-	
-	if i~=-1 then ColorsTable[j][i]=concat(a,"|") else ColorsTable[j]=concat(a,"|") end
-end
+local Colors,ColorsIdx,VarColors,VarNames,Check,CheckVar,Measure,Meter,MinValue,MaxValue,MaxMin,Amp,Option,random,concat=
+	{},{},{},{},{},{},{},{},{},{},{},{},{},math.random,table.concat
 
 function Initialize()
-	local SKIN,SELF=SKIN,SELF
-	Index,Limit,Output=SKIN:ParseFormula(SELF:GetOption("Index")),SKIN:ParseFormula(SELF:GetOption("Limit")),SELF:GetOption("Output")
-
-	local Sub,Index,Limit,Output,find=SELF:GetOption("Sub"),Index,Limit,Output,string.find
+	local SKIN,SELF,SplitColors=SKIN,SELF,{} Index,Limit,Output=SKIN:ParseFormula(SELF:GetOption("Index")),SKIN:ParseFormula(SELF:GetOption("Limit")),SELF:GetOption("Output")
+	local Sub,Index,Limit,Output,find,gsub,gmatch,ceil=SELF:GetOption("Sub"),Index,Limit,Output,string.find,string.gsub,string.gmatch,math.ceil
 	for j=1,Output do
-		ColorsOptTable[j],VarNameTable[j]=SELF:GetOption("Colors"..j),SELF:GetOption("VarName"..j)
+		SplitColors[j],Colors[j],ColorsIdx[j],VarColors[j],CheckVar[j],VarNames[j]={},{},{},{},{},SELF:GetOption("VarName"..j)
 		MinValue[j],MaxValue[j]=SKIN:ParseFormula(SELF:GetOption("MinimumValue"..j,0)),SKIN:ParseFormula(SELF:GetOption("MaximumValue"..j,1))
-		MaxMin[j],Amp[j],Option[j]=math.ceil(MaxValue[j]-MinValue[j]),SKIN:ReplaceVariables(SELF:GetOption("MeasureAmp"..j,1)),SELF:GetOption("MeterOption"..j)
+		MaxMin[j],Amp[j],Option[j]=ceil(MaxValue[j]-MinValue[j]),SKIN:ReplaceVariables(SELF:GetOption("MeasureAmp"..j,1)),SELF:GetOption("MeterOption"..j)
 		
-		local Measure,Meter=SELF:GetOption("MeasureName"..j),SELF:GetOption("MeterName"..j)
-		if find(Measure,Sub) then MeasureTable[j]={} MeasureSubTable[j]=1 for i=Index,Limit do MeasureTable[j][i]=SKIN:GetMeasure((gsub(Measure,Sub,i))) end
-		else MeasureTable[j]=SKIN:GetMeasure(SELF:GetOption("MeasureName"..j)) end
-		if find(Meter,Sub) then MeterTable[j]={} for i=Index,Limit do MeterTable[j][i]=(gsub(Meter,Sub,i)) end
-		else MeterTable[j]=SELF:GetOption("MeterName"..j) end
+		local MeasureName,MeterName=SELF:GetOption("MeasureName"..j),SELF:GetOption("MeterName"..j)
+		if find(MeasureName,Sub) then Measure[j]={} Check[j]=1 for i=Index,Limit do Measure[j][i]=SKIN:GetMeasure((gsub(MeasureName,Sub,i))) end
+			else Measure[j]=SKIN:GetMeasure(MeasureName) end
+		if find(MeterName,Sub) then Meter[j]={} for i=Index,Limit do Meter[j][i]=(gsub(MeterName,Sub,i)) end else Meter[j]=SELF:GetOption(MeterName) end
+		if find(VarNames[j],Sub) then Check[j]=2 for i=Index,Limit do VarColors[j][i]={} end end
 		
-		local ColorsStr=ColorsOptTable[j]
-		if find(ColorsStr,Sub) then ColorsCheckTable[j]=1 end
-		if find(VarNameTable[j],Sub) then VarTable[j]={} VarSubTable[j]=1 ColorsTable[j]={} local Go=Go
-			for i=Index,Limit do Go(ColorsStr,1,i,j) end
-		elseif find(ColorsStr,"Random") then ColorsCheckTable[j]=2 Go(ColorsStr,1,-1,j) end
-	end
-	for j=1,Output do ColorsVarTable[j]={} local ColorsStr=ColorsOptTable[j] for k=1,Output do if find(ColorsStr,VarNameTable[k]) then ColorsVarTable[j][k]=1 end end end
-end
+		local ColorsStr,a=SELF:GetOption("Colors"..j),1 for b in gmatch(ColorsStr,"[^%|]+") do SplitColors[j][a]=b a=a+1 end
+		for a=1,2 do Colors[j][a]={} ColorsIdx[j][a]={} local b=1 for c in gmatch(SplitColors[j][a],"[^,]+") do ColorsIdx[j][a][b]=c b=b+1 end
+			for d=1,4 do if ColorsIdx[j][a][d]=="Random" then Colors[j][a][d]=random(0,255) else Colors[j][a][d]=ColorsIdx[j][a][d] end end
+			for k=1,Output do if SplitColors[j][a]==VarNames[k] then if Check[k]==2 then CheckVar[j][a]=1 end
+				Colors[j][a]=VarNames[k] ColorsIdx[j][a]=VarNames[k] break end end end end end
 
-function Transition(j)
-	if VarSubTable[j] then local Go,Index,Limit=Go,Index,Limit
-		for i=Index,Limit do Go(ColorsTable[j][i],-1,i,j) end
-	else Go(ColorsTable[j],-1,-1,j) end
-end
+function Transition(j) for a=1,4 do Colors[j][1][a]=Colors[j][2][a] if ColorsIdx[j][2][a]=="Random" then Colors[j][2][a]=random(0,255) else Colors[j][2][a]=ColorsIdx[j][2][a] end end end
 
 function Update()
-	local function Check(j)
-		local ColorsVarTable,MeterTable=ColorsVarTable[j],MeterTable[j]
-		local MinValue,MaxValue,MaxMin,Amp,Option=MinValue[j],MaxValue[j],MaxMin[j],Amp[j],Option[j]
-		
-		local function CheckAgain(ColorsStr,Output,i)
-			local function Main(ColorsStr,MeasureTable,i)
-				local function Calculate(MeasureValue,Colors)
-					local function Average(a,b,c,d) local Value=((a*(d-c)+(b and b or 0)*c)/d)+0.5 return Value-Value%1 end
-					local function FastAverage(a,b,c) local Value=(a*(1-c)+(b and b or 0)*c)+0.5 return Value-Value%1 end
-					
-					if MaxMin==1 then
-						local uColor,rValue={},MeasureValue-MinValue
-						for i=1,4 do uColor[i]=FastAverage(Colors[i][1],Colors[i][2],rValue) end
-						return concat(uColor,",")
-					else
-						local uColor,rValue={},MeasureValue-MinValue
-						local NumCalc=rValue/MaxMin local Num=NumCalc-NumCalc%1
-						for i=1,4 do uColor[i]=Average(Colors[i][Num+1],Colors[i][Num+2],rValue%MaxMin,MaxMin) end
-						return concat(uColor,",")
-					end
-				end
-				
-				local function AmpCheck(MeasureValue)
-					local AmpValue=Amp*MeasureValue if AmpValue>MaxValue then return MaxValue else return AmpValue end
-				end
-
-				local gmatch,Colors,f=gmatch,{},0
-				for a=1,4 do Colors[a]={} end
-				for a in gmatch(ColorsStr,"[^%|]+") do
-					local b,e={},0 f=f+1
-					for c in gmatch(a,"[^,]+") do e=e+1 b[e]=c end
-					for d=1,4 do Colors[d][f]=b[d] and b[d] or 255 end end
-					
-				if i~=-1 then
-					if MeterTable ~= "" then SKIN:Bang("!SetOption",MeterTable[i],Option,Calculate(AmpCheck(MeasureTable[i]:GetValue()),Colors))
-					else VarTable[j][i]=Calculate(i,Colors) end
-				else VarTable[j]=Calculate(AmpCheck(MeasureTable:GetValue()),Colors) end
-			end
-		
-			if i~=-1 then local MeasureTable=MeasureTable for k=1,Output do
-				if ColorsVarTable[k] then ColorsStr=(gsub(ColorsStr,VarNameTable[k],VarTable[k])) elseif MeasureSubTable[k] then MeasureTable=MeasureTable[k] end end
-				if MeasureTable[i]:GetValue()~=0 then Main(ColorsStr,MeasureTable[j],i) end
-			elseif ColorsCheckTable[j]==1 then
-				local a,b,MeasureTable,ColorsStrLocal,Index,Limit={},0,MeasureTable[j],ColorsStrLocal,Index,Limit
-				for k=1,Output do if ColorsVarTable[k] then if VarSubTable[k] then a[b]=k b=b+1
-					else ColorsStr=(gsub(ColorsStr,VarNameTable[k],VarTable[k])) end end end
-				for m=Index,Limit do
-					if MeasureTable[m]:GetValue()~=0 then for c=0,b-1 do ColorsStrLocal=(gsub(ColorsStr,VarNameTable[a[c]],VarTable[a[c]][m])) end
-						Main(ColorsStrLocal,MeasureTable,m) end end
-			else 
-				for k=1,Output do if ColorsVarTable[k] then ColorsStr=(gsub(ColorsStr,VarNameTable[k],VarTable[k])) end end
-				if MeasureSubTable[j] then local MeasureTable,Index,Limit=MeasureTable[j],Index,Limit for i=Index,Limit do
-					if MeasureTable[i]:GetValue()~=0 then Main(ColorsStr,MeasureTable,i) end end
-				else Main(ColorsStr,MeasureTable[j],-1) end
-			end
-		end
-		
-		if VarSubTable[j] then local ColorsTable,Index,Limit=ColorsTable[j],Index,Limit for i=Index,Limit do CheckAgain(ColorsTable[i],Output,i) end
-		elseif ColorsCheckTable[j]==2 then CheckAgain(ColorsTable[j],Output,-1)
-		else CheckAgain(ColorsOptTable[j],Output,-1) end
-	end
-	
-	for j=1,Output do Check(j) end
-end
+	for j=1,Output do local Colors,CheckVar,Meter,MinValue,MaxValue,MaxMin,Amp,Option,Index,Limit,concat=
+		Colors[j],CheckVar[j],Meter[j],MinValue[j],MaxValue[j],MaxMin[j],Amp[j],Option[j],Index,Limit,concat
+		local function Main(Colors1,Colors2,Measure,i)
+			if i~=-1 then if Meter~="" then local AmpValue=Amp*Measure[i]:GetValue() if AmpValue>MaxValue then AmpValue=MaxValue-.0001 end
+					local ColorsTable,c={},(AmpValue-MinValue)%MaxMin local b=MaxMin-c
+					for k=1,4 do local Value=((Colors1[k]*b+(Colors2[k] and Colors2[k] or 0)*c)/MaxMin)+0.5 ColorsTable[k]=Value-Value%1 end
+					SKIN:Bang("!SetOption",Meter[i],Option,concat(ColorsTable,","))
+				else local c=(i-MinValue)%MaxMin local b=MaxMin-c
+				for k=1,4 do local Value=((Colors1[k]*b+(Colors2[k] and Colors2[k] or 0)*c)/MaxMin)+0.5 VarColors[j][i][k]=Value-Value%1 end end
+			else local c=(Measure:GetValue()-MinValue)%MaxMin local b=MaxMin-c
+			for k=1,4 do local Value=((Colors1[k]*b+(Colors2[k] and Colors2[k] or 0)*c)/MaxMin)+0.5 VarColors[j][k]=Value-Value%1 end end end
+			
+		local ColorsTable={} for a=1,2 do for k=1,Output do if Colors[a]==VarNames[k] then ColorsTable[a]=VarColors[k] break end end
+			if ColorsTable[a]==nil then ColorsTable[a]={} for b=1,4 do ColorsTable[a][b]=Colors[a][b] end end end
+		if Check[j] or CheckVar[1] or CheckVar[2] then local Measure,Output=Measure,Output for k=Output,1,-1 do if Check[k]==1 then Measure=Measure[k] break end end
+			if CheckVar[1] and CheckVar[2] then for i=Index,Limit do if Measure[i]:GetValue()~=0 then Main(ColorsTable[1][i],ColorsTable[2][i],Measure,i) end end
+			elseif CheckVar[1] then for i=Index,Limit do if Measure[i]:GetValue()~=0 then Main(ColorsTable[1][i],ColorsTable[2],Measure,i) end end
+			elseif CheckVar[2] then for i=Index,Limit do if Measure[i]:GetValue()~=0 then Main(ColorsTable[1],ColorsTable[2][i],Measure,i) end end
+			else for i=Index,Limit do if Measure[i]:GetValue()~=0 then Main(ColorsTable[1],ColorsTable[2],Measure,i) end end end
+		else Main(ColorsTable[1],ColorsTable[2],Measure[j],-1) end end end
