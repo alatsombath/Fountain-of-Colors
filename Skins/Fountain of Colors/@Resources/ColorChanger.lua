d8ba213edb9f7db095f3d76791fe51bc79f7c7a4
alatsombath <alatsombath@users.noreply.github.com>
2015-11-27 14:00:16 -0800

    3.4.6 (27 November 2015)
    - Added fountain effects (experimental) (enabled by default)
    - Decay threshold: Fading in threshold (default: 49)
    - Decay sustain: Determines how long before the bars fade out (default
    66 ms)
    - Decay duration: Determines how fast the bars fade out (default: 660
    ms)
    - (Removed old opacity options)
    - Adjusted default settings:
    - Decreased AverageSize (past values to decrease stuttering) for dynamic
    Sensitivity/FFTDecay from 72 (1.15 seconds) to 56 (0.9 seconds)
    - Increased smoothness (AverageSize = 3)
    - Decreased decay speed (FFTDecay = 250)
    - Increased number of bars (Bands = 49)
    - Increased horizontal blending (hBlendingMultiplier = 0.4)

--- Skins/Fountain of Colors/@Resources/ColorChanger.lua
+++ Skins/Fountain of Colors/@Resources/ColorChanger.lua
@@ -1,4 +1,4 @@
--- ColorChanger v3.0.3, A modification of ColorChanger v1.3 by Smurfier
+-- ColorChanger v3.1, A modification of ColorChanger v1.3 by Smurfier
 -- LICENSE: Creative Commons Attribution-Non-Commercial-Share Alike 3.0
 
 function Initialize()
@@ -7,15 +7,21 @@ function Initialize()
   color, colorIdx, hColorIdx, hPosNorm, cacheColor = {}, {}, {}, {}, {}
   for b = 1, 8 do hColorIdx[b] = {} end
   measure, meterName, option = {}, {}, SELF:GetOption("MeterOption")
+  oldMeasureValues, timeSinceDecay, decayEffect = {}, {}, SELF:GetNumberOption("DecayEffect")
+  decayThreshold = 0.001 * SELF:GetNumberOption("DecayThreshold")
+  decaySustain, decayDuration = 0.001 * 62.5 * SELF:GetNumberOption("DecaySustain"), 0.001 * 62.5 * SELF:GetNumberOption("DecayDuration")
   hInvert = SELF:GetNumberOption("hInvert")
   hBlendingMultiplier, vBlendingMultiplier = SELF:GetNumberOption("hBlendingMultiplier"), SELF:GetNumberOption("vBlendingMultiplier")
   opacityMultiplier, opacityLower, opacityUpper = SELF:GetNumberOption("OpacityMultiplier"), SELF:GetNumberOption("OpacityLower"), SELF:GetNumberOption("OpacityUpper")
   hLowerLimit, hUpperLimit = SELF:GetNumberOption("hLowerLimit") + 1, SELF:GetNumberOption("hUpperLimit") + 1
+  
   for i = hLowerLimit, hUpperLimit do
-    cacheColor[i], colorIdx[i], hPosNorm[i] = 0, {}, hInvert == 0 and (i / hUpperLimit) * hBlendingMultiplier or (1 - (i / hUpperLimit)) * hBlendingMultiplier
+    oldMeasureValues[i], timeSinceDecay[i] = 0, 0
+	cacheColor[i], colorIdx[i], hPosNorm[i] = "", {}, hInvert == 0 and (i / hUpperLimit) * hBlendingMultiplier or (1 - (i / hUpperLimit)) * hBlendingMultiplier
 	measure[i], meterName[i] = SKIN:GetMeasure(SELF:GetOption("MeasureBaseName") .. i-1), SELF:GetOption("MeterBaseName") .. i-1
 	for c = 1, 4 do colorIdx[i][c] = {} end
   end
+  
   updateWhenZero = SELF:GetNumberOption("UpdateWhenZero")
   transitionTime = math.ceil(SELF:GetOption("TransitionTime") * 1000 / 16)
   enableTransition, enableHorizontalTransition = 0, 0
@@ -95,28 +101,53 @@ function Update()
     elseif parent ~= 0 then Transition()
 	else return 0 end
   end
+
   for i = hLowerLimit, hUpperLimit do
     if measure[i]:GetValue() ~= 0 or updateWhenZero ~= 0 then
+	  local measureValue = measure[i]:GetValue()
 	  local color, colorIdx, counterNorm = color, colorIdx[i], counterNorm[counter]
-	  local blendingValue, opacityValue = vBlendingMultiplier * measure[i]:GetValue(), opacityMultiplier * measure[i]:GetValue()
+	  local blendingValue = vBlendingMultiplier * measureValue
 	  if blendingValue > 1 then blendingValue = 1 end
 	  local blendingValueRe, counterNormRe = 1 - blendingValue, 1 - counterNorm
+	  
 	  if enableTransition ~= 0 then
         for a = 1, 3 do
           color[a] = floor((colorIdx[1][a] * counterNormRe + colorIdx[3][a] * counterNorm) * blendingValueRe + (colorIdx[2][a] * counterNormRe + colorIdx[4][a] * counterNorm) * blendingValue + 0.5)
         end
 	  else
 	    for a = 1, 3 do 
-		  color[a] = floor(colorIdx[1][a] * blendingValueRe + colorIdx[2][a] * blendingValue + 0.5)
-		end
+	      color[a] = floor(colorIdx[1][a] * blendingValueRe + colorIdx[2][a] * blendingValue + 0.5)
+	    end
       end
-	  if opacityValue > 1 then opacityValue = 1 end
-      color[4] = floor(opacityLower * (1 - opacityValue) + opacityUpper *  opacityValue + 0.5)
+	  
+	  if decayEffect ~= 0 then
+	    diff, oldMeasureValues[i] = measureValue - oldMeasureValues[i], measureValue
+	    if diff > decayThreshold or measureValue > 1 - diff then
+		  timeSinceDecay[i], color[4] = -1, 255
+        else
+          timeSinceDecay[i] = timeSinceDecay[i] + 1
+	      if timeSinceDecay[i] > decaySustain then
+	        if timeSinceDecay[i] > decayDuration then
+		      timeSinceDecay[i], color[4] = decayDuration, 0
+		    else
+		      local decay = timeSinceDecay[i] - decaySustain
+		      if decay < 0 then decay = 0 end
+		      color[4] = 255 * (1 - (decay / decayDuration))
+		    end
+	      else
+		    color[4] = 255
+	      end
+	    end
+	  end
+
 	  color = concat(color, ",")
 	  if color ~= cacheColor[i] then
 	    cacheColor[i] = color
-		SKIN:Bang("!SetOption", meterName[i], option, color)
+	    SKIN:Bang("!SetOption", meterName[i], option, color)
 	  end
+	  
+	else
+	  oldMeasureValues[i] = 0
 	end
   end
 end
